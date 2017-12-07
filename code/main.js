7298dafe88daec70e2b4e8e8d92772970318d4fa
Rafael Quirino <rafael.david.quirino@gmail.com>
2017-12-07 11:09:28 -0200

    Atualizando codigo...

--- code/main.js
+++ code/main.js
@@ -1,9 +1,39 @@
-function play() {
+var GAME = {
+    paused: false,
+    pause_img: 'pause.png',
+    resume_img: 'resume.png'
+};
 
+function play() {
+    //alert("Clicked PLAY");
+    document.getElementById('main_screen').style.visibility = 'hidden';
+    document.getElementById('game_menu').style.visibility = 'visible';
+    document.getElementById('game_screen').style.visibility = 'visible';
 }
 
-var SpriteSheet = function(url,offset) {
-    this.url = url;
-    this.offset = offset;
-    
+function records() {
+    alert("Clicked RECORDS");
+}
+
+function pause() {
+    var pause_img = document.getElementById('pause_img');
+    var pause_txt = document.getElementById('pause_txt');
+    if (GAME.paused) {
+        pause_img.src = GAME.pause_img;
+        pause_txt.innerHTML = 'PAUSE (P)';
+        GAME.paused = false;
+    }
+    else {
+        pause_img.src = GAME.resume_img;
+        pause_txt.innerHTML = 'RESUME (P)';
+        GAME.paused = true;
+    }
+}
+
+function restart() {
+    alert('Clicked RESTART');
+}
+
+function exit() {
+    alert('Clicked EXIT');
 }
