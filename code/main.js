99465c50e705fc949919db66efe5a96f217fe048
Rafael Quirino <rafael.david.quirino@gmail.com>
2017-12-07 12:39:42 -0200

    sistema de escuta de eventos de teclado

--- code/main.js
+++ code/main.js
@@ -1,18 +1,30 @@
+var MAIN_SCREEN         = 0;
+var GAME_SCREEN         = 1;
+var RECORDS_SCREEN      = 2;
+var NEWRECORD_SCREEN    = 3;
+var MYRECORD_SCREEN     = 4;
+
 var GAME = {
+    screen: MAIN_SCREEN,
     paused: false,
     pause_img: 'pause.png',
-    resume_img: 'resume.png'
+    resume_img: 'resume.png',
+    keys_down: {
+        p: false,
+        r: false,
+        e: false
+    }
 };
 
 function play() {
-    //alert("Clicked PLAY");
     document.getElementById('main_screen').style.visibility = 'hidden';
     document.getElementById('game_menu').style.visibility = 'visible';
     document.getElementById('game_screen').style.visibility = 'visible';
+    GAME.screen = GAME_SCREEN;
 }
 
 function records() {
-    alert("Clicked RECORDS");
+    //alert("Clicked RECORDS");
 }
 
 function pause() {
@@ -31,11 +43,72 @@ function pause() {
 }
 
 function restart() {
-    alert('Clicked RESTART');
+    //alert('Clicked RESTART');
+    var pause_img = document.getElementById('pause_img');
+    var pause_txt = document.getElementById('pause_txt');
+    pause_img.src = GAME.pause_img;
+    pause_txt.innerHTML = 'PAUSE (P)';
+    GAME.paused = false;
 }
 
 function exit() {
     document.getElementById('main_screen').style.visibility = 'visible';
     document.getElementById('game_menu').style.visibility = 'hidden';
     document.getElementById('game_screen').style.visibility = 'hidden';
+    GAME.screen = MAIN_SCREEN;
 }
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+
+    if (GAME.screen == MAIN_SCREEN) {
+        if (keyName == 'p' || keyName == 'P') {
+            if (!GAME.keys_down.p) {
+                play();
+                GAME.keys_down.p = true;
+            }
+        }
+        if (keyName == 'r' || keyName == 'R') {
+            if (!GAME.keys_down.r) {
+                records();
+                GAME.keys_down.r = true;
+            }
+        }
+    }
+
+    if (GAME.screen == GAME_SCREEN) {
+        if (keyName == 'p' || keyName == 'P') {
+            if (!GAME.keys_down.p) {
+                pause();
+                GAME.keys_down.p = true;
+            }
+        }
+        if (keyName == 'r' || keyName == 'R') {
+            if (!GAME.keys_down.r) {
+                restart();
+                GAME.keys_down.r = true;
+            }
+        }
+        if (keyName == 'e' || keyName == 'E') {
+            if (!GAME.keys_down.e) {
+                exit();
+                GAME.keys_down.e = true;
+            }
+        }
+    }
+
+}, false);
+
+document.addEventListener('keyup', (event) => {
+    const keyName = event.key;
+
+    if (keyName == 'p' || keyName == 'P') {
+        GAME.keys_down.p = false;
+    }
+    if (keyName == 'r' || keyName == 'R') {
+        GAME.keys_down.r = false;
+    }
+    if (keyName == 'e' || keyName == 'E') {
+        GAME.keys_down.e = false;
+    }
+}, false);
